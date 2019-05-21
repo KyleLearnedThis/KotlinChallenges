53f7ee2c4ea791e2b662308345fcc89e83cabac4
Kyle Learned <kyle.learned.this@gmail.com>
2019-05-21 01:04:54 -0700

    TreeDepth solution and test.

---
+++ src/main/kotlin/com/albion/labs/tree/bst/TreeDepth.kt
@@ -0,0 +1,30 @@
+package com.albion.labs.tree.bst
+
+import com.albion.tree.bst.TreeNode
+
+class TreeDepth {
+    fun minDepth(root: TreeNode<Int>?): Int {
+        if (root == null) {
+            return 0
+        }
+        val left = root.left
+        val right = root.right
+        val l = minDepth(left)
+        val r = minDepth(right)
+
+        return if(l == 0 || r == 0) {
+            l + r + 1
+        } else {
+            Math.min(l,r) + 1
+        }
+    }
+
+    fun maxDepth(root: TreeNode<Int>?): Int {
+        if (root == null) {
+            return 0
+        }
+        val left = root.left
+        val right = root.right
+        return Math.max(maxDepth(left), maxDepth(right)) + 1
+    }
+}
