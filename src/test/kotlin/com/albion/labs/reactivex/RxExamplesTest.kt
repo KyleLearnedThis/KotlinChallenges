c4840f2e28bae30acf59402f6acbb21d951b7693
KyleLearnedThis <kyle.learned.this@gmail.com>
2019-12-08 23:43:46 -0800

    Added RxExamples, refactoring (#27)

--- src/test/kotlin/com/albion/labs/reactivex/RxExamplesTest.kt
+++ src/test/kotlin/com/albion/labs/reactivex/RxExamplesTest.kt
@@ -6,11 +6,21 @@ class RxExamplesTest {
 
     @Test
     fun testExample0001() {
-        RxExamples.example001()
+        RxExamples.testFlatmap()
     }
 
     @Test
     fun testExample0002() {
-        RxExamples.example002()
+        RxExamples.testZip()
+    }
+
+    @Test
+    fun testExample0003() {
+        RxExamples.testCombineLastInterval()
+    }
+
+    @Test
+    fun testExample0004() {
+        RxExamples.testReduce()
     }
 }
