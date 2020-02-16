fe6fbc432f9a54a102915815a6e8661e4a7fe868
KyleLearnedThis <kyle.learned.this@gmail.com>
2020-02-15 21:51:05 -0800

    Basic Http GET and Json parser via Gson (#31)

---
+++ src/main/kotlin/com/albion/labs/lang/network/HttpClientExample.kt
@@ -0,0 +1,36 @@
+package com.albion.labs.lang.network
+
+import com.albion.labs.lang.network.data.ChuckNorrisJoke
+import com.google.gson.Gson
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+class HttpClientExample {
+    fun httpGet(urlString: String): String {
+        val client = HttpClient.newBuilder().build()
+        val request = HttpRequest.newBuilder()
+                .uri(URI(urlString))
+                .build()
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+        return response.body().toString()
+    }
+
+    fun rxHttpGet(urlString: String) {
+        Observable.fromCallable { httpGet(urlString) }
+                .subscribeOn(Schedulers.io())
+                .subscribe {
+                    print("====== $it ======")
+                }
+    }
+
+    fun httpGetChuckNorrisJoke(urlString: String) {
+        val input = httpGet(urlString)
+        val parser = Gson()
+        val joke = parser.fromJson<ChuckNorrisJoke>(input, ChuckNorrisJoke::class.java)!!
+        println("===== ${joke.value} =====")
+    }
+}
