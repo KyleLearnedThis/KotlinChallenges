fd0c3305e6ef92f96663b3484ff0b44b903582e3
Kyle Learned <kyle.learned.this@gmail.com>
2021-04-22 00:44:36 -0700

    Repeated String count character 'a' in occurrence, solution and test.

--- src/main/kotlin/com/albion/labs/texts/ReverseOnlyLetters.kt
+++ src/main/kotlin/com/albion/labs/texts/ReverseOnlyLetters.kt
@@ -2,17 +2,20 @@ package com.albion.labs.texts
 
 import java.util.*
 
+
 // https://leetcode.com/problems/reverse-only-letters/
 class ReverseOnlyLetters {
     companion object {
         fun reverseOnlyLetters(S: String): String {
+            val array = S.toCharArray()
             val letters = Stack<Char>()
-            for (c in S.toCharArray())
+            for (c in array) {
                 if (Character.isLetter(c)) {
                     letters.push(c)
                 }
+            }
             val ans = StringBuilder()
-            for (c in S.toCharArray()) {
+            for (c in array) {
                 if (Character.isLetter(c)) {
                     ans.append(letters.pop())
                 } else {
