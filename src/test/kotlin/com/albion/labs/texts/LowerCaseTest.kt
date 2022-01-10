19bc6283b26a92fb0cb52fa159ea13735884bb66
Kyle Learned <kyle.learned.this@gmail.com>
2022-01-10 02:39:34 -0800

    MissingNumber solution and test cases, code refactoring.

--- src/test/kotlin/com/albion/labs/texts/LowerCaseTest.kt
+++ src/test/kotlin/com/albion/labs/texts/LowerCaseTest.kt
@@ -13,4 +13,13 @@ class LowerCaseTest {
         val actual = lowerCase.toLowerCase(input)
         Assert.assertEquals(actual, expected)
     }
+
+    @Test
+    fun testToLowerCaseV2() {
+        val input = "HoRaTiO"
+        val expected = "horatio"
+        val lowerCase = LowerCase()
+        val actual = lowerCase.toLowerCaseV2(input)
+        Assert.assertEquals(actual, expected)
+    }
 }
