2fb55497b29a86896a513132d48c46a5fa828539
KyleLearnedThis <kyle.learned.this@gmail.com>
2022-01-21 02:45:52 -0800

    MatchPairSumSet, hashing solution and tests (#76)

--- src/test/kotlin/com/albion/labs/numbers/MatchPairSumSetTest.kt
+++ src/test/kotlin/com/albion/labs/numbers/MatchPairSumSetTest.kt
@@ -22,6 +22,21 @@ class MatchPairSumSetTest {
     }
 
     @Test
+    fun testBasicV2() {
+        val a = intArrayOf(1, 2, 4, 5, 7)
+        val b = intArrayOf(5, 6, 3, 4, 8)
+        val n = 9
+        val hold = arrayOf(
+            intArrayOf(1,8),
+            intArrayOf(4,5),
+            intArrayOf(5,4)
+        )
+        val expected = makeExpectedPairArray(hold)
+        val actual = MatchPairSumSet.allPairsV2(a,b,n)
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
     fun testNegativeNumberCase() {
         val a = intArrayOf(-1, -2, 4, -6, 5, 7)
         val b = intArrayOf(6, 3, 4, 0)
@@ -32,6 +47,20 @@ class MatchPairSumSetTest {
         )
         val expected = makeExpectedPairArray(hold)
         val actual = MatchPairSumSet.allPairs(a,b,n)
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testNegativeNumberCaseV2() {
+        val a = intArrayOf(-1, -2, 4, -6, 5, 7)
+        val b = intArrayOf(6, 3, 4, 0)
+        val n = 8
+        val hold = arrayOf(
+            intArrayOf(4,4),
+            intArrayOf(5,3)
+        )
+        val expected = makeExpectedPairArray(hold)
+        val actual = MatchPairSumSet.allPairsV2(a,b,n)
         assertArrayEquals(expected, actual)
     }
 
