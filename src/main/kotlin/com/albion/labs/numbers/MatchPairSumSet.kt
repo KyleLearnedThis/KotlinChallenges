a11fac6aaeb027d84201a7413e771ec924d2f95f
Kyle Learned <kyle.learned.this@gmail.com>
2022-01-21 02:45:23 -0800

    MatchPairSumSet, hashing solution and tests

--- src/main/kotlin/com/albion/labs/numbers/MatchPairSumSet.kt
+++ src/main/kotlin/com/albion/labs/numbers/MatchPairSumSet.kt
@@ -3,6 +3,7 @@ package com.albion.labs.numbers
 // https://practice.geeksforgeeks.org/problems/find-all-pairs-whose-sum-is-x5808/1
 class MatchPairSumSet {
     companion object {
+        // Basic solution O(n) = n^2
         fun allPairs(a: IntArray, b: IntArray, n: Int): Array<Pair<Int, Int>> {
             val hold = mutableListOf<Pair<Int, Int>>()
             for (l in a) {
@@ -13,7 +14,27 @@ class MatchPairSumSet {
                     }
                 }
             }
-            return hold.toTypedArray()
+            val result = hold.toTypedArray()
+            result.sortBy { it.first }
+            return result
+        }
+
+        // Hashing --> O(max(n, m))
+        fun allPairsV2(a: IntArray, b: IntArray, n: Int): Array<Pair<Int, Int>> {
+            val map = mutableMapOf<Int,Int>()
+            val hold = mutableListOf<Pair<Int, Int>>()
+            for(i in a) {
+                map[i] = 0
+            }
+            for(j in b) {
+                val rem = n - j
+                if(map.containsKey(rem)) {
+                    hold.add(Pair(rem, j))
+                }
+            }
+            val result = hold.toTypedArray()
+            result.sortBy { it.first }
+            return result
         }
     }
 }
